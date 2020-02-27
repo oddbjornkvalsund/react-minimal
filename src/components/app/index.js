564d1eb11e3d5c5f2712a072e09cd59d06306dc9
Oddbjørn Kvalsund <oddbjorn.kvalsund@gmail.com>
2020-02-27 10:52:06 +0100

    Add typescript support

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
+import {Hello} from "../tstest/index.tsx";
+
 export const App = (props) => {
   return (
-	<h1>Hello Oddbjørn!</h1>
+		<div>
+			<h1>Hello Oddbjørn!</h1>
+			<Hello compiler="TypeScript" framework="React" />
+		</div>
   );
 }
